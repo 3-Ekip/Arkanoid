eb13685120a9a8c2239a9b162c220fca38631942
MrOrganixPro <mehmetakifergani1@gmail.com>
2025-04-30 17:23:05 +0300

    17:22 Tuesday Change

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public int StartForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rb.AddForce(new Vector2(0, StartForce));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    
+        
+
 }
