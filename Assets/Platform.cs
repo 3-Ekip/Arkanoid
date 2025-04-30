afbc292648fcc80c321db8848c1b22e36851fbc9
Horanta <yedihilaloyun1@gmail.com>
2025-04-30 19:07:29 +0300

    buton eklendi

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     public float speed = 5f;
+    private int direction = 0;
+    public float maxX = 1.355f;
+    public float  negativemaxX = -1.355f;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (direction != 0)
+        {
+            transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
+
+            
+            float clampedX = Mathf.Clamp(transform.position.x, negativemaxX, maxX);
+            transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+        }
+
+        Debug.Log(direction);
+
+
+
+
+
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    
+
+    public void ToggleDirection()
     {
-        rb.AddForce(new Vector2 (-2, 0));
+        if (direction == 0)
+        {
+            direction = 1;  
+        }
+        else
+        {
+            direction *= -1;  
+        }
     }
+
+
 }
